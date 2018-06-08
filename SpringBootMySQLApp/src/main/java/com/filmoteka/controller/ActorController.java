9e6ee3cc1dfcad27eadd91df118678e32480f890
Sylwia Bartosiak <201718@edu.p.lodz.pl>
2018-06-08 16:26:15 +0200

    Dodanie kontrolera aktora i nagrody

--- SpringBootMySQLApp/src/main/java/com/filmoteka/controller/ActorController.java
+++ SpringBootMySQLApp/src/main/java/com/filmoteka/controller/ActorController.java
@@ -16,5 +16,43 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class ActorController {
-    
+    @Autowired
+    ActorService actorService;
+    @RequestMapping(value = "/actors/{id}" , method = RequestMethod.GET)
+    ResponseEntity<Actor> getActor(@PathVariable("id") Long id){
+        try{
+            Actor actor = new Actor(actorService.getById(id));
+            return new ResponseEntity<Actor>(actor,HttpStatus.OK);
+        }catch (EntityNotFoundException e){
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+    }
+    @RequestMapping(value = "/actors/{id}" , method = RequestMethod.DELETE)
+    public void deleteActor(@PathVariable("id") Long id){
+        actorService.deleteById(id);
+    }
+    @RequestMapping(value = "/actors", method = RequestMethod.POST)
+    public ResponseEntity <Actor> createActor (@RequestBody Actor actor){
+        try {
+            Actor newActor = actorService.createActor(actor);
+            return new ResponseEntity<Actor>(newActor, HttpStatus.OK);
+        }
+        catch (Exception c){
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @RequestMapping(value ="/actors/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Actor> updateActor (@PathVariable("id") Long id, @RequestBody Actor actor){
+        try{
+            Actor actor1 = actorService.updateActor(actor,id);
+            return new ResponseEntity<Actor>(actor1,HttpStatus.OK);
+        }
+        catch (Exception c){
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @RequestMapping(value="/actors", method=RequestMethod.GET)
+    public List<Actor> getAllActors() {
+        return actorService.getAllActors();
+    }
 }
--- SpringBootMySQLApp/src/main/java/com/filmoteka/controller/AwardController.java
+++ SpringBootMySQLApp/src/main/java/com/filmoteka/controller/AwardController.java
@@ -17,4 +17,42 @@ import java.util.List;
 
 public class AwardController {
 
+    @Autowired
+    AwardService awardService;
+    @RequestMapping(value = "/awards/{id}", method = RequestMethod.GET)
+    ResponseEntity<Award> getAward(@PathVariable("id") Long id) {
+        try {
+            Award award = new Award(awardService.getById(id));
+            return new ResponseEntity<Award>(award, HttpStatus.OK);
+        } catch (EntityNotFoundException e) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+    }
+    @RequestMapping(value = "/awards/{id}", method = RequestMethod.DELETE)
+    public void deleteActor(@PathVariable("id") Long id) {
+        awardService.deleteById(id);
+    }
+    @RequestMapping(value = "/awards", method = RequestMethod.POST)
+    public ResponseEntity<Award> createActor(@RequestBody Award award) {
+        try {
+            Award newAward = awardService.createAward(award);
+            return new ResponseEntity<Award>(newAward, HttpStatus.OK);
+        } catch (Exception c) {
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @RequestMapping(value = "/awards/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Award> updateAward(@PathVariable("id") Long id, @RequestBody Award award) {
+        try {
+            Award award1 = awardService.updateAward(award, id);
+            return new ResponseEntity<Award>(award1, HttpStatus.OK);
+        } catch (Exception c) {
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @RequestMapping(value = "/awards", method = RequestMethod.GET)
+    public List<Award> getAllAwards() {
+        return awardService.getAllAwards();
+    }
+
 }
