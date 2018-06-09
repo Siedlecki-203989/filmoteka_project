3a1d51b473fbbf11c0db0ff16dd2b18f0708008f
Bartek <gradek96@o2.pl>
2018-06-09 13:37:18 +0200

    Dodanie kontrolera użytkownika

--- SpringBootMySQLApp/src/main/java/com/filmoteka/controller/UserController.java
+++ SpringBootMySQLApp/src/main/java/com/filmoteka/controller/UserController.java
@@ -1,10 +1,58 @@
 package com.filmoteka.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.filmoteka.sdo.User;
+import com.filmoteka.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api")
 
 public class UserController {
+
+    @Autowired
+    UserService userService;
+    @RequestMapping(value = "/users/{id}" , method = RequestMethod.GET)
+    ResponseEntity<User> getUser(@PathVariable("id") Long id){
+        try{
+            User user = new User(userService.getById(id));
+            return new ResponseEntity<User>(user, HttpStatus.OK);
+        }catch (EntityNotFoundException e){
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+    }
+    @RequestMapping(value = "/users/{id}" , method = RequestMethod.DELETE)
+    public void deleteUser(@PathVariable("id") Long id){
+        userService.deleteById(id);
+    }
+    @RequestMapping(value = "/users", method = RequestMethod.POST)
+    public ResponseEntity <User> createUser (@RequestBody User user){
+        try {
+            User newUser = userService.createUser(user);
+            return new ResponseEntity<User>(newUser, HttpStatus.OK);
+        }
+        catch (Exception c){
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @RequestMapping(value ="/users/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<User> updateUser (@PathVariable("id") Long id, @RequestBody User user){
+        try{
+            User user1 = userService.updateUser(user,id);
+            return new ResponseEntity<User>(user1,HttpStatus.OK);
+        }
+        catch (Exception c){
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @RequestMapping(value="/users", method=RequestMethod.GET)
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
 }
--- SpringBootMySQLApp/src/main/java/com/filmoteka/service/UserService.java
+++ SpringBootMySQLApp/src/main/java/com/filmoteka/service/UserService.java
@@ -1,4 +1,71 @@
 package com.filmoteka.service;
 
+import com.filmoteka.Exceptions.IncorrectIdException;
+import com.filmoteka.dao.User;
+import com.filmoteka.repository.UserRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class UserService {
+
+    final Logger logger = LogManager.getLogger(ActorService.class);
+
+    @Autowired
+    UserRepository userRepository;
+
+    public User getById(Long id){
+        return userRepository.getOne(id);
+    }
+
+
+    public void deleteById(Long id){
+        try {
+            userRepository.deleteById(id);
+        }
+        catch(Exception e){
+            logger.warn("IllegalArgumentException in application");
+        }
+    }
+
+    public com.filmoteka.sdo.User createUser (com.filmoteka.sdo.User user){
+        User u = new User();
+        u.setCity(user.getCity());
+        u.setLogin(user.getLogin());
+        u.setPassword(user.getPassword());
+        u.setUserName(user.getUserName());
+        u = userRepository.save(u);
+        com.filmoteka.sdo.User saved = new com.filmoteka.sdo.User(u);
+        return saved;
+    }
+
+    public com.filmoteka.sdo.User updateUser (com.filmoteka.sdo.User user, Long id) throws IncorrectIdException {
+
+        User user1 = new User();
+        if(!user.getId().equals(id)) {
+            throw new IncorrectIdException("Wrong id!");
+        }
+        user1 = userRepository.getOne(id);
+        user1.setUserName(user.getUserName());
+        user1.setPassword(user.getPassword());
+        user1.setLogin(user.getLogin());
+        user1.setCity(user.getCity());
+        user1 = userRepository.save(user1);
+        com.filmoteka.sdo.User saved = new com.filmoteka.sdo.User(user1);
+        return saved;
+    }
+
+    public List<com.filmoteka.sdo.User> getAllUsers(){
+        List<com.filmoteka.sdo.User>userList= new ArrayList<>();
+        for (User userDao:userRepository.findAll()) {
+            com.filmoteka.sdo.User user = new com.filmoteka.sdo.User(userDao);
+            userList.add(user);
+        }
+        return userList;
+    }
 }
