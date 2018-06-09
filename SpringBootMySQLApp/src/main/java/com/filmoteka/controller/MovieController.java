696cc73527ba17540d7382dabdf8c410fe223f09
Bartek <gradek96@o2.pl>
2018-06-09 15:26:05 +0200

    Dodanie kontrolera filmu i gatunku

--- SpringBootMySQLApp/src/main/java/com/filmoteka/controller/MovieController.java
+++ SpringBootMySQLApp/src/main/java/com/filmoteka/controller/MovieController.java
@@ -1,10 +1,58 @@
 package com.filmoteka.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.filmoteka.sdo.Movie;
+import com.filmoteka.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api")
 
 public class MovieController {
+
+    @Autowired
+    MovieService movieService;
+    @RequestMapping(value = "/movies/{id}" , method = RequestMethod.GET)
+    ResponseEntity<Movie> getMovie(@PathVariable("id") Long id){
+        try{
+            Movie movie = new Movie(movieService.getById(id));
+            return new ResponseEntity<Movie>(movie, HttpStatus.OK);
+        }catch (EntityNotFoundException e){
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+    }
+    @RequestMapping(value = "/movies/{id}" , method = RequestMethod.DELETE)
+    public void deleteMovie(@PathVariable("id") Long id){
+        movieService.deleteById(id);
+    }
+    @RequestMapping(value = "/movies", method = RequestMethod.POST)
+    public ResponseEntity <Movie> createMovie (@RequestBody Movie movie){
+        try {
+            Movie newMovie = movieService.createMovie(movie);
+            return new ResponseEntity<Movie>(newMovie, HttpStatus.OK);
+        }
+        catch (Exception c){
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @RequestMapping(value ="/movies/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Movie> updateMovie (@PathVariable("id") Long id, @RequestBody Movie movie){
+        try{
+            Movie movie1 = movieService.updateMovie(movie,id);
+            return new ResponseEntity<Movie>(movie1,HttpStatus.OK);
+        }
+        catch (Exception c){
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @RequestMapping(value="/movies", method=RequestMethod.GET)
+    public List<Movie> getAllMovies() {
+        return movieService.getAllMovies();
+    }
 }
--- SpringBootMySQLApp/src/main/java/com/filmoteka/service/GenreService.java
+++ SpringBootMySQLApp/src/main/java/com/filmoteka/service/GenreService.java
@@ -1,4 +1,66 @@
 package com.filmoteka.service;
 
+import com.filmoteka.Exceptions.IncorrectIdException;
+import com.filmoteka.dao.Genre;
+import com.filmoteka.repository.GenreRepository;
+import com.filmoteka.sdo.User;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class GenreService {
+
+    final Logger logger = LogManager.getLogger(GenreService.class);
+
+    @Autowired
+    GenreRepository genreRepository;
+
+    public Genre getById(Long id){
+        return genreRepository.getOne(id);
+    }
+
+
+    public void deleteById(Long id){
+        try {
+            genreRepository.deleteById(id);
+        }
+        catch(Exception e){
+            logger.warn("IllegalArgumentException in application");
+        }
+    }
+
+    public com.filmoteka.sdo.Genre createGenre (com.filmoteka.sdo.Genre genre){
+        Genre g = new Genre();
+        g.setName(genre.getName());
+        g = genreRepository.save(g);
+        com.filmoteka.sdo.Genre saved = new com.filmoteka.sdo.Genre(g);
+        return saved;
+    }
+
+    public com.filmoteka.sdo.Genre updateGenre (com.filmoteka.sdo.Genre genre, Long id) throws IncorrectIdException {
+
+        Genre genre1 = new Genre();
+        if(!genre.getId().equals(id)) {
+            throw new IncorrectIdException("Wrong id!");
+        }
+        genre1 = genreRepository.getOne(id);
+        genre1.setName(genre.getName());
+        genre1 = genreRepository.save(genre1);
+        com.filmoteka.sdo.Genre saved = new com.filmoteka.sdo.Genre(genre1);
+        return saved;
+    }
+
+    public List<com.filmoteka.sdo.Genre> getAllGenres(){
+        List<com.filmoteka.sdo.Genre>genreList= new ArrayList<>();
+        for (Genre genreDao: genreRepository.findAll()) {
+            com.filmoteka.sdo.Genre genre = new com.filmoteka.sdo.Genre(genreDao);
+            genreList.add(genre);
+        }
+        return genreList;
+    }
 }
