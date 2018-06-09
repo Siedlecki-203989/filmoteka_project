f68da1dfd6ec7c0c14a4ac018f0f33c14f6df9ac
Sylwia Bartosiak <201718@edu.p.lodz.pl>
2018-06-09 18:05:05 +0200

    Testy w aktorze - get + put. Kolejna proba podpiecia travisa

--- SpringBootMySQLApp/src/test/java/com/filmoteka/controller/ActorControllerTest.java
+++ SpringBootMySQLApp/src/test/java/com/filmoteka/controller/ActorControllerTest.java
@@ -1,5 +1,7 @@
 package com.filmoteka.controller;
 
+import com.filmoteka.sdo.Actor;
+import io.restassured.http.ContentType;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,7 +14,9 @@ public class ActorControllerTest {
     @Test
     public void getActor() {
         get("/api/actors/1").then().statusCode(200).assertThat()
-                .body("firstName", equalTo("Jason"));
+                .body("firstName", equalTo("Jason"))
+                .body("lastName", equalTo("Statham"))
+                .body("id", equalTo(1));
     }
 
     @Test
@@ -25,6 +29,22 @@ public class ActorControllerTest {
 
     @Test
     public void updateActor() {
+
+        Actor actor = new Actor();
+        actor.setId((long) 2);
+        actor.setFirstName ("Katarzyna");
+        actor.setLastName ("Figura");
+
+        given().body (actor)
+                .when ()
+                .contentType (ContentType.JSON)
+                .put ("/api/actors/2");
+
+        get("/api/actors/2").then().statusCode(200).assertThat()
+                .body("firstName", equalTo("Katarzyna"))
+                .body("lastName", equalTo("Figura"))
+                .body("id", equalTo(2));
+
     }
 
     @Test
