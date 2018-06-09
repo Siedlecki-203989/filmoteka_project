dbb6a5df6acab16dc726efa3dc934b8e9df7c913
Bartek <gradek96@o2.pl>
2018-06-09 16:20:45 +0200

    Kontroler recenzji

--- SpringBootMySQLApp/src/main/java/com/filmoteka/controller/ReviewController.java
+++ SpringBootMySQLApp/src/main/java/com/filmoteka/controller/ReviewController.java
@@ -1,10 +1,58 @@
 package com.filmoteka.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.filmoteka.sdo.Review;
+import com.filmoteka.service.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api")
 
 public class ReviewController {
+
+    @Autowired
+    ReviewService reviewService;
+    @RequestMapping(value = "/reviews/{id}" , method = RequestMethod.GET)
+    ResponseEntity<Review> getReview(@PathVariable("id") Long id){
+        try{
+            Review review = new Review(reviewService.getById(id));
+            return new ResponseEntity<Review>(review, HttpStatus.OK);
+        }catch (EntityNotFoundException e){
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+    }
+    @RequestMapping(value = "/reviews/{id}" , method = RequestMethod.DELETE)
+    public void deleteReview(@PathVariable("id") Long id){
+        reviewService.deleteById(id);
+    }
+    @RequestMapping(value = "/reviews", method = RequestMethod.POST)
+    public ResponseEntity <Review> createReview (@RequestBody Review review){
+        try {
+            Review newReview = reviewService.createReview(review);
+            return new ResponseEntity<Review>(newReview, HttpStatus.OK);
+        }
+        catch (Exception c){
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @RequestMapping(value ="/reviews/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Review> updateReview (@PathVariable("id") Long id, @RequestBody Review review){
+        try{
+            Review review1 = reviewService.updateReview(review,id);
+            return new ResponseEntity<Review>(review1,HttpStatus.OK);
+        }
+        catch (Exception c){
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @RequestMapping(value="/reviews", method=RequestMethod.GET)
+    public List<Review> getAllReview() {
+        return reviewService.getAllReviews();
+    }
 }
